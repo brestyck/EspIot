b4fde28ada5db7d1f39aee6d32769a19be71e692
brestyck <47777597+brestyck@users.noreply.github.com>
2021-03-06 20:22:33 +0300

    Улучшено оформление

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ except urllib.error.HTTPError:
 
 print("\u001b[39mAsking server about second ESP....")
 try:
-    print("Got answer: \u001b[32m"+urllib.request.urlopen("https://chat-retriever.herokuapp.com/get/metadata/esp").read().decode("utf-8"))
+    print("ESP CID: \u001b[32m"+urllib.request.urlopen("https://chat-retriever.herokuapp.com/get/metadata/esp").read().decode("utf-8"))
     sss_esp_addr = urllib.request.urlopen("https://chat-retriever.herokuapp.com/get/metadata/esp").read().decode("utf-8")
 
 except urllib.error.HTTPError:
@@ -26,10 +26,12 @@ except urllib.error.HTTPError:
 
 #ASKING ESPS.......
 while True:
-    s_esp_addr = "https://chat-retriever.herokuapp.com/get/data/" + str(sss_esp_addr)#concat with id
-    f_esp_addr = "https://chat-retriever.herokuapp.com/get/data/" + str(fss_esp_addr)#concat with id
-    f_esp_sens = "https://chat-retriever.herokuapp.com/get/metadata/" + str(fss_esp_addr)#for rewriting in cycle
-    s_esp_sens = "https://chat-retriever.herokuapp.com/get/metadata/" + str(sss_esp_addr)#same
+    try:
+        s_esp_addr = "https://chat-retriever.herokuapp.com/get/data/" + str(sss_esp_addr)#concat with id
+        f_esp_addr = "https://chat-retriever.herokuapp.com/get/data/" + str(fss_esp_addr)#concat with id
+        f_esp_sens = "https://chat-retriever.herokuapp.com/get/metadata/" + str(fss_esp_addr)#for rewriting in cycle
+        s_esp_sens = "https://chat-retriever.herokuapp.com/get/metadata/" + str(sss_esp_addr)#same
+    except:print("\u001b[41mALL ESP'S ARE OFFLINE!\u001b[49m");os.system("figlet -f small OFFLINE | lolcat")
     try:
         f_esp_dt = urllib.request.urlopen(f_esp_addr).read().decode("utf-8")#fetching data
         f_esp_sens = urllib.request.urlopen(f_esp_sens).read().decode("utf-8")#fetching sens
@@ -39,9 +41,9 @@ while True:
         s_esp_sens = urllib.request.urlopen(s_esp_sens).read().decode("utf-8")#fetching sens
     except:s_esp_dt="unaval";s_esp_sens="unaval"
     print("\n")
-    print("\u001b[39mESP\u001b[32m"+fss_esp_addr+"\n\u001b[39msens:\u001b[35m"+f_esp_sens+"\n\u001b[39mdata: \u001b[31m"+f_esp_dt)
+    print("\u001b[31m"+fss_esp_addr+"\n\u001b[39mSensor type:\u001b[33m"+f_esp_sens+"\n\u001b[39mValue: \u001b[32m"+f_esp_dt)
     print("\n")
-    print("\u001b[39mESP\u001b[32m"+sss_esp_addr+"\n\u001b[39msens:\u001b[35m"+s_esp_sens+"\n\u001b[39mdata: \u001b[31m"+s_esp_dt)
+    print("\u001b[31m"+sss_esp_addr+"\n\u001b[39mSensor type:\u001b[33m"+s_esp_sens+"\n\u001b[39mValue: \u001b[32m"+s_esp_dt)
     time.sleep(20)
     os.system("clear")
     os.system("cowsay -f eyes Watch your data, master... | lolcat")
