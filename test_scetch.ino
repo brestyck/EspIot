c32b77530864a0777213219a5f1480ac2dda504e
brestyck <47777597+brestyck@users.noreply.github.com>
2021-03-14 12:22:04 +0300

    Тестовый режим для АЦП MCP3008

--- test_scetch.ino
+++ test_scetch.ino
@@ -1,18 +1,27 @@
-// GET запрос к http://api.openweathermap.org
+#define CS_PIN 15
+#define MOSI_PIN 13
+#define MISO_PIN 12
+#define CLOCK_PIN 14
+#include <MCP3008.h>
+
 const String esp_id = "ofBoris";
 
 #include <ESP8266WiFi.h>
 const String sensor_type = "Photoresistor"; // Какой датчик подключаем
+const String sensor_type1 = "Thermoresistor"; // Какой датчик подключаем
 const char* ssid     = "ssid";         // тут SSID и пароль к WIFI
 const char* password = "pass";
 
 const char* host = "chat-retriever.herokuapp.com";     // тут адрес сервера
-
+MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 
 void setup() {
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(1000);
   Serial.begin(115200);
   delay(10);
-  pinMode(0, INPUT);
   pinMode(16, OUTPUT);
 
   // в сетапе как обычно подключаемся к сети
@@ -39,13 +48,20 @@ void setup() {
   Get("GET /deploy/esp/"+esp_id+"/VIRTUALHOST HTTP/1.1");        // при  включении выполняем функцию
 }
 int data = 0;
+int data1 = 0;
 void loop() {
-  delay(30000);
-  data = analogRead(0);
+  delay(20000);
+  data = adc.readADC(0);
+  delay(1000);
+  data1 = adc.readADC(1);
   delay(1000);
   Get("GET /delete/"+esp_id+" HTTP/1.1");
   delay(1000);
   Get("GET /deploy/"+esp_id+"/"+(String)data+"/"+sensor_type+" HTTP/1.1");
+  delay(1000);
+  Get("GET /delete/VIRTUALHOST HTTP/1.1");
+  delay(1000);
+  Get("GET /deploy/VIRTUALHOST/"+(String)data1+"/"+sensor_type1+" HTTP/1.1");
 }
 
 
